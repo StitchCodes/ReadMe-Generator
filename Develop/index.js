9ff02b1147095ec23ab8e7e1f177b21efea96020
StitchCodes <coderstitch@gmail.com>
2022-01-19 22:14:56 -0600

    Added initial inquier prompts. Working on file write and readme creation

--- Develop/index.js
+++ Develop/index.js
@@ -8,27 +8,64 @@ const questions = ["Proyect Name: ", "Description: ", "Contents: ", "Installatio
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  console.log("We are ready to generate your ReadMe. Please answer a few questions");
 
-// Function call to initialize app
-init();
-
-
-
-// ----------------------
-/*
-inquirer
+  inquirer
   .prompt([
-    // Pass your questions in here
+    {
+      name:"title",
+      message: questions[0]
+    },
+    {
+      name:"description",
+      message: questions[1]
+    },
+    {
+      name:"contents",
+      message: questions[2]
+    },
+    {
+      name:"installation",
+      message: questions[3]
+    },
+    {
+      name:"usage",
+      message: questions[4]
+    },
+    {
+      name:"license",
+      message: questions[5]
+    },
+    {
+      name:"license",
+      message: questions[5]
+    },
+    {
+      name:"contributing",
+      message: questions[6]
+    },
+    {
+      name:"tests",
+      message: questions[7]
+    },
+    {
+      name:"questions",
+      message: questions[8]
+    }
   ])
   .then((answers) => {
-    // Use user feedback for... whatever!!
+    writeToFile("readMe.md", JSON.stringify(answers))
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
+      console.log("Error. Try again.")
     } else {
-      // Something else went wrong
+      console.log("Error. We couldn't process your request")
     }
   });
-/*
+
+}
+
+// Function call to initialize app
+init();
