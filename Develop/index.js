f1c974915d092f22007174388f4e0c4605f20ad2
StitchCodes <coderstitch@gmail.com>
2022-01-26 10:00:01 -0600

    Working markdown

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,7 @@ const inquirer = require('inquirer');
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = ["Proyect Name: ", "Description: ", "Contents: ", "Installation: ", "Usage: ", "License: ", "Contributing: ", "Tests: ", "Questions: "];
+const questions = ["Proyect Name: ", "Description: ", "Contents: ", "Installation: ", "Usage: ", "License: ", "Contributors: ", "Tests: ", "Questions: "];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,6 +13,14 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## ${data.description}
+  ## Contents: ${data.contents}
+  ## Run the following commands in your terminal to Install the app: ${data.installation}
+  ## To use this app please follow this steps: ${data.usage}
+  ## License: ${data.license}
+  ## Developed by: ${data.contributing}
+  ## The following test were performed to ensure the functinability of this app: ${data.tests}
+  ## For questions please reach me at ${data.questions}
 
 `;
 }
